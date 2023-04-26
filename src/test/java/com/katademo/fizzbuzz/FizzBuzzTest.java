780d7bfa808c29d61da88a1d9779d58d619099af
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-04-26 18:47:07 +0530

    refactor(fizzbuzz): refactoring a fizzbuzz test case refactored the fizzbuzz first requirement to make it more readable and maintainable

--- src/test/java/com/katademo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/katademo/fizzbuzz/FizzBuzzTest.java
@@ -10,12 +10,39 @@ import org.junit.Test;
  *
  */
 public class FizzBuzzTest {
+	
+	private FizzBuzz fizzBuzz = new FizzBuzz(); 
 
 	@Test
-	public void testFizzBuzz() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("1", fizzBuzz.convert(1));
-		assertEquals("2", fizzBuzz.convert(2));
-		assertEquals("Fizz", fizzBuzz.convert(3));
-	}
+    public void shouldReturnNumber() {
+        assertEquals("1", fizzBuzz.convert(1));
+    }
+
+    @Test
+    public void shouldReturnFizz() {
+        assertEquals("Fizz", fizzBuzz.convert(3));
+    }
+
+    @Test
+    public void shouldReturnBuzz() {
+        assertEquals("Buzz", fizzBuzz.convert(5));
+    }
+
+    @Test
+    public void shouldReturnFizzBuzz() {
+        assertEquals("FizzBuzz", fizzBuzz.convert(15));
+    }
+    
+    @Test
+    public void shouldReturnNumberForOthers() {
+        assertEquals("7", fizzBuzz.convert(7));
+    }
+
+    @Test
+    public void shouldPrintFizzBuzzFrom1To100() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz.convert(i));
+        }
+    }
+    
 }
