b3e4ccc9f5c214217e09e72d668ef39a8f4d216e
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-04-26 19:07:49 +0530

    refactor(fizzbuzztest) : Refactor and add the fizzbuzz logic for requirement two REFACTORED and ADDED a test case for second requirement

--- src/test/java/com/katademo/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/katademo/fizzbuzz/FizzBuzzTest.java
@@ -10,39 +10,47 @@ import org.junit.Test;
  *
  */
 public class FizzBuzzTest {
-	
-	private FizzBuzz fizzBuzz = new FizzBuzz(); 
+
+	private FizzBuzz fizzBuzz = new FizzBuzz();
 
 	@Test
-    public void shouldReturnNumber() {
-        assertEquals("1", fizzBuzz.convert(1));
-    }
+	public void shouldReturnNumber() {
+		assertEquals("1", fizzBuzz.convert(1));
+		assertEquals("1", fizzBuzz.convertWithContains(1));
+	}
 
-    @Test
-    public void shouldReturnFizz() {
-        assertEquals("Fizz", fizzBuzz.convert(3));
-    }
+	@Test
+	public void shouldReturnFizz() {
+		assertEquals("Fizz", fizzBuzz.convert(3));
+		assertEquals("Fizz", fizzBuzz.convertWithContains(3));
+	}
 
-    @Test
-    public void shouldReturnBuzz() {
-        assertEquals("Buzz", fizzBuzz.convert(5));
-    }
+	@Test
+	public void shouldReturnBuzz() {
+		assertEquals("Buzz", fizzBuzz.convert(5));
+		assertEquals("Buzz", fizzBuzz.convertWithContains(5));
+	}
 
-    @Test
-    public void shouldReturnFizzBuzz() {
-        assertEquals("FizzBuzz", fizzBuzz.convert(15));
-    }
-    
-    @Test
-    public void shouldReturnNumberForOthers() {
-        assertEquals("7", fizzBuzz.convert(7));
-    }
+	@Test
+	public void shouldReturnFizzBuzz() {
+		assertEquals("FizzBuzz", fizzBuzz.convert(15));
+	}
 
-    @Test
-    public void shouldPrintFizzBuzzFrom1To100() {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(fizzBuzz.convert(i));
-        }
-    }
-    
+	@Test
+	public void shouldReturnNumberForOthers() {
+		assertEquals("7", fizzBuzz.convert(7));
+	}
+
+	@Test
+	public void shouldPrintFizzBuzzFrom1To100() {
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(fizzBuzz.convert(i));
+		}
+	}
+
+	@Test
+	public void shouldPrintFizzBuzzBuzz() {
+		assertEquals("FizzBuzzBuzz", fizzBuzz.convertWithContains(35));
+	}
+
 }
